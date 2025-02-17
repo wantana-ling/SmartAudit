187789fed10c052025576b23099a9a4eeb47c7d0
mimie888 <onlyonexx.ap@gmail.com>
2025-02-17 22:10:16 +0700

    k

---
+++ src/pages/t.jsx
@@ -0,0 +1,21 @@
+function LoginPage() {
+    return (
+        <div className="container">
+            <div className="box-container">
+                <div className="form-contaniner">
+                    {/* <img src="" alt="" /> */}
+                    <div className="text">
+                        <h1>Login</h1>
+                        <p>Smart Audit</p>
+                    </div>
+                    <form action="/audit" method="post">
+                        <input type="text" name="username" placeholder="Username" required/>
+                        <input type="text" name="password" placeholder="Password" required/>
+                        <p class="link-text"><a href="#">Forget Password?</a></p>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
